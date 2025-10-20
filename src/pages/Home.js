b3540097244421e89bf095eae3e738b7d0b5f2d2
jlmunozpb <jlmunozpb@gmail.com>
2025-10-20 00:15:17 -0500

    delete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { api } from '../services/api';
 import { Title } from '../components/Title';
 import { Input } from '../components/Input';
@@ -7,101 +7,33 @@ import { FiPlus, FiSearch } from "react-icons/fi";
 import Swal from 'sweetalert2';
 import '../assets/styles/scss/pages/Home.scss';
 
-const handleDeleteProperty = () => {
-      Swal.fire({
-        title: "Eliminar Propiedad",
-        text: "Estás seguro?",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Si, eliminar",
-        cancelButtonText: "Cancelar"
-      }).then((result) => {
-        if (result.isConfirmed) {
-
+const handleDeleteProperty = (propertyId) => {
+    Swal.fire({
+      title: "Eliminar Propiedad",
+      text: "Estás seguro?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar",
+      cancelButtonText: "Cancelar"
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const response = await api.delete(`/api/property/${propertyId}`);
+        
+        if(200 <= response && response <= 299) {
           Swal.fire({
             title: "Propiedad Eliminada",
             text: "La propiedad ha sido eliminada con éxito",
             icon: "success"
           });
         }
-      });
-
-  // Swal.fire({
-  //   html: `
-  //     <div class="swal-container">
-  //       <label for="swal-name">Nombre</label>
-  //       <input id="swal-name" class="swal2-input" placeholder="Nombre">
-        
-  //       <label for="swal-date">Fecha de entrega</label>
-  //       <input id="swal-date" type="date" class="swal2-input">
-
-  //       <label for="swal-time">Hora de entrega</label>
-  //       <input id="swal-time" type="time" class="swal2-input">
-
-  //       <label for="swal-message">Mensaje</label>
-  //       <textarea id="swal-message" class="swal2-textarea" placeholder="Mensaje"></textarea>
-  //     </div>
-  //   `,
-  //   focusConfirm: false,
-  //   confirmButtonText: "Crear",
-  //   customClass: {
-  //     confirmButton: 'home-accept-btn',
-  //     popup: 'home-swal-popup',
-  //   },
-  //   preConfirm: () => {
-  //     const nameTask = document.getElementById('swal-name').value;
-  //     const dateTask = document.getElementById('swal-date').value;
-  //     const timeTask = document.getElementById('swal-time').value;
-  //     const messageTask = document.getElementById('swal-message').value;
-
-  //     if (!nameTask || !dateTask || !timeTask || !messageTask) {
-  //       Swal.showValidationMessage(`Por favor completa todos los campos`);
-  //       return false;
-  //     }
-
-  //     return { nameTask, dateTask, timeTask, messageTask };
-  //   }
-  // }).then(async (result) => {
-  //   if (result.isConfirmed) {
-  //     try {
-  //       const response = await api.post(`/api/property`, {
-  //         name: result.value.nameTask,
-  //         date: result.value.dateTask,
-  //         time: result.value.timeTask,
-  //         message: result.value.messageTask,
-  //       });
-
-  //       if (response.status >= 200 && response.status < 300) {
-  //         Swal.fire({
-  //           icon: "success",
-  //           title: "Tarea creada",
-  //           html: `
-  //             <div style="text-align: left; margin-left: 5vw;">
-  //               <p><strong>Nombre:</strong> ${result.value.nameTask}</p>
-  //               <p><strong>Fecha:</strong> ${result.value.dateTask}</p>
-  //               <p><strong>Hora:</strong> ${result.value.timeTask}</p>
-  //               <p><strong>Mensaje:</strong> ${result.value.messageTask}</p>
-  //             </div>
-  //           `
-  //         });
-  //       }
-
-  //     } catch (error) {
-  //       Swal.fire({
-  //         text: error.response?.data?.error?.message || error.message,
-  //         icon: "error"
-  //       });
-  //       console.error('Error al crear tarea:', error);
-  //     }
-  //   }
-  // });
+      }
+    });
 };
 
 export const Home = () => {
   const navigate = useNavigate();
-  const { propertyId } = useParams();
   const [properties, setProperties] = useState([]);
   const [queryPropertyName, setQueryPropertyName] = useState("");
   const [pagination, setPagination] = useState({
@@ -143,14 +75,11 @@ export const Home = () => {
     fetchItems();
   }, [pagination, navigate, userId]);
 
-  const handleOpenProperty = (propertyId) => {
-    navigate(`/api/property/${propertyId}`);
-  };
-
   const handleUpdateProperty = (propertyId) => {
     navigate(`/crud-property/${propertyId}`);
   };
 
+  /** Pagination */
   const handleNextPage = () => {
     if (pagination.page < pagination.last_page) {
       setPagination(prev => ({ ...prev, page: prev.page + 1 }));
